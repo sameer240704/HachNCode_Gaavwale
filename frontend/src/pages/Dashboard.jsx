4f3046dce6c9bbe35b7cfde23853d0ed75902f42
AditiAmbasta13 <aaddiittii1133@gmail.com>
2024-04-06 21:42:53 +0530

    ok ok

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useSidebarState } from "../hooks/useSidebarState";
+import BasicCards from "../components/BasicCard.jsx"
 
 const Dashboard = () => {
   const { expanded } = useSidebarState();
   return (
-    <div
-      className="absolute top-0"
-      style={{ left: expanded ? "20vw" : "4vw" }}
-    ></div>
+    <div>
+      <h1 className="absolute text-white text-3xl font-semibold mt-20 right-20">Dashboard</h1>
+      <div
+        className="absolute top-0"
+        style={{ left: expanded ? "20vw" : "4vw" }}
+      ></div>
+      <div className="absolute w-64 h-40 bg-yellow-200 top-52">
+        <h1>Card1</h1>
+      </div>
+
+  </div>
+
   );
 };
 
