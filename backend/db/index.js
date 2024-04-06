2f1a73a757dfc56f3fafda5eccfb5036e52e465b
parthpsg <66634291+parthgala1@users.noreply.github.com>
2024-04-06 17:49:10 +0530

    backend creation

---
+++ backend/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}`
+    );
+    console.log(
+      "MongoDB connected!! DB HOST: ",
+      connectionInstance.connection.host
+    );
+  } catch (error) {
+    console.log("Error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
-const express = require("express");
-const app = express();
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-app.listen;
+dotenv.config({ path: "./.env" });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}!`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+  });
