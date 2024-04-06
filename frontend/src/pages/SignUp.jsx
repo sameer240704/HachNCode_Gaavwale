8a106b8850deafc5aacc185a0e12312eddccc4c1
parthpsg <66634291+parthgala1@users.noreply.github.com>
2024-04-07 01:48:44 +0530

    Shayad everything solved

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -16,23 +16,8 @@ const SignUp = () => {
 		password: "",
 		confirmPassword: ""
 	});
-	const [inputs, setInputs] = useState({
-		name: "",
-		username: "",
-		email: "",
-		standard: "",
-		school: "",
-		Profile: "",
-		password: "",
-		confirmPassword: "",
-		gender: "",
-	});
 
 	const { loading, signup } = useSignup();
-
-	const handleCheckboxChange = (gender) => {
-		setInputs({ ...inputs, gender });
-	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -91,9 +76,9 @@ const SignUp = () => {
 								type="email"
 								placeholder="Enter email"
 								className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-								value={inputs.username}
+								value={inputs.email}
 								onChange={(e) =>
-									setInputs({ ...inputs, username: e.target.value })
+									setInputs({ ...inputs, email: e.target.value })
 								}
 							/>
 						</div>
@@ -109,7 +94,7 @@ const SignUp = () => {
 								className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
 								value={inputs.standard}
 								onChange={(e) =>
-									setInputs({ ...inputs, username: e.target.value })
+									setInputs({ ...inputs, standard: e.target.value })
 								}
 							/>
 						</div>
@@ -123,9 +108,9 @@ const SignUp = () => {
 								type="text"
 								placeholder="Enter college"
 								className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-								value={inputs.College}
+								value={inputs.school}
 								onChange={(e) =>
-									setInputs({ ...inputs, username: e.target.value })
+									setInputs({ ...inputs, school: e.target.value })
 								}
 							/>
 						</div>
@@ -203,106 +188,6 @@ const SignUp = () => {
 						</div>
 					</form>
 				</div>
-			</div>
-
-			<div>
-				<label className="label p-2 ">
-					<span className="text-base label-text text-yellow-200">Username</span>
-				</label>
-				<input
-					type="text"
-					placeholder="johndoe"
-					className="rounded-lg w-full input input-bordered h-10 px-4 bg-gray-600 text-green-300"
-					value={inputs.username}
-					onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-				/>
-			</div>
-			<div>
-				<label className="label p-2 ">
-					<span className="text-base label-text text-yellow-200">Email</span>
-				</label>
-				<input
-					type="email"
-					placeholder="johndoe"
-					className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-					value={inputs.username}
-					onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-				/>
-			</div>
-			<div>
-				<label className="label p-2 ">
-					<span className="text-base label-text text-yellow-200">Standard</span>
-				</label>
-				<input
-					type="email"
-					placeholder="johndoe"
-					className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-					value={inputs.username}
-					onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-				/>
-			</div>
-			<div>
-				<label className="label p-2 ">
-					<span className="text-base label-text text-yellow-200">College</span>
-				</label>
-				<input
-					type="email"
-					placeholder="johndoe"
-					className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-					value={inputs.username}
-					onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-				/>
-			</div>
-
-			<div>
-				<label className="label">
-					<span className="text-base label-text text-yellow-200">Password</span>
-				</label>
-				<input
-					type="password"
-					placeholder="Enter Password"
-					className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-					value={inputs.password}
-					onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-				/>
-			</div>
-
-			<div>
-				<label className="label">
-					<span className="text-base label-text text-yellow-200">
-						Confirm Password
-					</span>
-				</label>
-				<input
-					type="password"
-					placeholder="Confirm Password"
-					className="rounded-lg w-full input input-bordered px-4 h-10 bg-gray-600 text-green-300"
-					value={inputs.confirmPassword}
-					onChange={(e) =>
-						setInputs({ ...inputs, confirmPassword: e.target.value })
-					}
-				/>
-			</div>
-
-			<Link
-				to={"/login"}
-				className="text-sm hover:underline text-yellow-200 hover:text-red-600 mt-2 inline-block"
-				href="#"
-			>
-				Already have an account?
-			</Link>
-
-			<div>
-				<button
-					className="rounded-lg btn btn-block btn-sm mt-2 border border-slate-700 text-gray-800 bg-white hover:text-yellow-200 border-none px-10 py-3"
-					disabled={loading}
-				>
-					{loading ? (
-						<span className="loading loading-spinner"></span>
-					) : (
-						"Sign Up"
-					)}
-				</button>
 			</div>
 		</div>
 	);
