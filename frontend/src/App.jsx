ddc52936b2a794fb1071cdda39ab0210faf7298f
AditiAmbasta13 <aaddiittii1133@gmail.com>
2024-04-06 18:12:27 +0530

    Landingg Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Homepage } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react'
+import Banner from "../components/Banner.jsx"
+import Navigation from "../components/Navigation"
+import Courses from "../components/Courses.jsx"
+import Contact from "../components/Contact.jsx"
 
 const Homepage = () => {
-  return <div>Homepage</div>;
-};
+  return (
+    <div>
+      <Navigation/>
+      <Banner/>
+      <Courses/>
+      <Contact/>
+    </div>
+  )
+}
 
-export default Homepage;
+export default Homepage
