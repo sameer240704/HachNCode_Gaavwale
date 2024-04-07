65c37b4cd1e984fe425c082c3696fda43ebbd8cf
sameer240704 <sameergupta79711@gmail.com>
2024-04-07 05:53:43 +0530

    made changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Homepage, Login, SignUp, Music } from "./pages";
 import { SidebarStateProvider } from "./hooks/useSidebarState";
 import Sidebar from "./components/Sidebar";
-``;
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Drums from "./components/Music/Drums";
 import { useAuthContext } from "./context/AuthContext";
+import useAarav from "./hooks/useAarav";
 
 function App() {
   const { authUser } = useAuthContext();
+  useAarav();
   return (
     <SidebarStateProvider>
       <BrowserRouter>
@@ -23,8 +25,14 @@ function App() {
               </>
             }
           />
-          <Route path="/login" element={authUser ? <Navigate to="/dashboard" /> : <Login />} />
-          <Route path="/signup" element={authUser ? <Navigate to="/dashboard" /> : <SignUp />} />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/dashboard" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/dashboard" /> : <SignUp />}
+          />
           <Route
             path="/music/drums"
             element={
@@ -44,6 +52,7 @@ function App() {
             }
           />
         </Routes>
+        <Toaster position="top-center" />
       </BrowserRouter>
     </SidebarStateProvider>
   );
